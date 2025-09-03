a1e3b706556b719230c1dbc9d00cb787acfcd673
12922114821 <natanoliveiraslv14@gmail.com>
2025-09-03 20:41:35 -0300

    fecth: assinatura da calculadora

---
+++ src/main/java/org/example/calculadora/Calculadora.java
@@ -0,0 +1,25 @@
+package org.example.calculadora;
+
+public class Calculadora {
+
+    int somar(int num, int num2) {
+        return 0;
+    }
+
+    int subtrair(int num, int num2) {
+        return 0;
+    }
+
+    int dividir(int num, int num2) {
+        return 0;
+    }
+
+    int multiplicar(int num, int num2) {
+        return 0;
+    }
+
+    int fatorial() {
+        return null;
+    }
+
+}
