ebb5b52fa7384c82b3460205909b8e8ab01cf73b
Natan Oliveira da Silva <12922114821@ulife.com.br>
2025-09-10 19:52:26 -0300

    envio de email de boas vindas com teste unitario

--- src/main/java/org/example/email/EmailManager.java
+++ src/main/java/org/example/email/EmailManager.java
@@ -4,9 +4,12 @@ public class EmailManager {
 
     private EmailService emailService;
 
-    public EmailManager(EmailService emailService){}
+    public EmailManager(EmailService emailService){
+        this.emailService = emailService;
+    }
 
-    public void sendWelcomEmail(String eamilAddress){
+    public void sendWelcomEmail(String to){
+        emailService.sendEmail(to, "Welcome!", "Welcome to our plataform.");
     }
 
 }
--- src/test/java/org/example/email/EmailManagerTest.java
+++ src/test/java/org/example/email/EmailManagerTest.java
@@ -1,12 +1,23 @@
 package org.example.email;
 
+import org.junit.Test;
+
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class EmailManagerTest {
 
-    EmailService mockEmailService = mock(EmailService.class);
+    @Test
+    public void shouldSendWelcomeEmail(){
+        // Arrange - Preparar
+        EmailService mockEmailService = mock(EmailService.class);
 
-    EmailManager emailManager = new EmailManager(mockEmailService);
+        EmailManager emailManager = new EmailManager(mockEmailService);
 
-    emailManager.sendWelcomEmail("natanoliveiraslv14@gmail.com");
+        // Action - Executar
+        emailManager.sendWelcomEmail("dickvigarista@gmail.com");
+
+        // Assert - Verificar
+        verify(mockEmailService).sendEmail("dickvigarista@gmail.com", "Welcome!", "Welcome to our plataform.");
+    }
 }
